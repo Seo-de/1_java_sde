0052d382dbaf9c192072603b71d3ba914b148b09
Seo-de <luejenie@gmail.com>
2022-08-12 17:30:00 +0900

    0812

--- 03_control/src/edu/kh/control/branch/BranchExample.java
+++ 03_control/src/edu/kh/control/branch/BranchExample.java
@@ -195,60 +195,66 @@ public class BranchExample {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("몇 판?");
-		int game = sc.nextInt();
-		
-		
-		for(int i=0; i<game; i++) {
+		int round = sc.nextInt();
+
+		// 승/무/패를 기록한 변수 선언 및 0으로 초기화
+		int win = 0;
+		int draw = 0;
+		int lose = 0;
+
+		for(int i=1; i<=round; i++) {
+			
+			System.out.println("\n"+i+"번째 게임");
 			System.out.print("가위/바위/보 중 하나를 입력 해주세요 : ");
 			String player = sc.next();
 			
-		
-			int rps = (int)(Math.random()*3);
+			// 컴픂터 가위/바위/보 지정
+			int ran = (int)(Math.random()*3); // 0가위 1바위 2보
 			
-			System.out.println("컴퓨터는 [" + rps + "]를 선택했습니다.");
+			String com = null;  // String의 기본 값
+			// null : 없다(비슷) -> 참조하지 않는다.
+			// "" = 그냥 비어있는 문자열을 뜻함. null과는 완전 다름.
 			
-			if(player.equals("가위")) {
-				if(rps == 0) {
-					System.out.println("비겼습니다.");
-				}else if(rps == 1) {
-					System.out.println("플레이어 승!");
-				}else {
-					System.out.println("졌습니다ㅠㅠ");
-				}
-	
-			}else if(player.equals("바위")) {
-				if(rps == 0) {
-					System.out.println("졌습니다ㅠㅠ");
-				}else if(rps == 1) {
-					System.out.println("비겼습니다.");
-				}else {
-					System.out.println("플레이어 승!");
-				}
-			} else {
-				if(rps == 0) {
-					System.out.println("플레이어 승!");
-				}else if(rps == 1) {
-					System.out.println("졌습니다ㅠㅠ");
-				}else {
-					System.out.println("비겼습니다.");
-				}
+			switch(ran) { // default 꼭 안써도 돼.
+			case 0 : com = "가위"; break;
+			case 1 : com = "바위"; break;
+			case 2 : com = "보"; break;
 			}
-	 
 			
-		}
-		
-		
-		
-		
-		
+			System.out.printf("컴퓨터 [%s]를 선택했습니다. \n", com);
+			
+			// 사용자와 컴퓨터 가위, 바위, 보 승패 판별
+			
+			if(player.equals(com)) {
+				System.out.println("비겼습니다.");
+				draw++;
+			} else {
+				
+				// 플레이어 - 컴퓨터
+				//  가위     보
+				//  바위     가위
+				//  보      바위
+				
+				// 사용자가 이기는 경우에 true가 되는 상황을 미리 변수로 선언
+				boolean win1 = player.equals("가위") && com.equals("보");
+				boolean win2 = player.endsWith("바위") && com.equals("가위");
+				boolean win3 = player.endsWith("보") && com.equals("바위");
 
-		
+				if(win1 || win2 || win3) {// 이기는 경우
+					System.out.println("플레이어 승!");
+					win++;
+				}else {// 지는 경우
+					System.out.println("졌습니다 ㅠㅠ");
+					lose++;
+				}
+				
+			}
+			
+			System.out.printf("현재 기록 : %d승 %d무 %d패 \n",win, draw, lose);
+
+		}
+
 	}
-	
-	
-	
-	
-	
 
 	
 }
